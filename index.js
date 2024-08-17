3e255eefadaecac40a2fa5deac11d8d888d904f7
Deepanagarjan <deepanagarajan1208@gmail.com>
2024-08-18 00:47:34 +0530

    node day1 commited

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+//importing space
+
+
+
+
+//declaration /intialisation
+const app = express();
+const PORT= 4000;
+//middleware
+app.use(express.json())
+
+
+//routes
+app.get('/',(req,res)=>{
+    // res.status(200).json{message:'Hi welcome to our first node app'}
+    res.status(200).send(`<span style="background-color:Aqua;color:black;font-size:100px">Welcome to our firstapp in nodejs </span>`);
+}
+)
+
+//running part
+app.listen(PORT,()=>{
+    console.log (`App is Listening on the PORT ${PORT}`)
+})
